0b8022caf6336b63b9ce5697b69a782488109838
YaraWS <39776511+YaraWS@users.noreply.github.com>
2023-08-27 11:45:44 +0100

    Add brand name on data.js

--- data.js
+++ data.js
@@ -1,6 +1,8 @@
-export const menuArray = [
+export const clothesArray = [
   {
       name: "Long sleeve shirt",
+      brand:"Ted Baker",
+      size:"S",
       material: ["90% cotton", "10% polyester"],
       id: 0,
       price: 14,
@@ -8,16 +10,20 @@ export const menuArray = [
   },
   {
       name: "Denin Shorts",
+      brand:"SuperDry",
+      size:"M",
       material: ["98% cotton", "2% elastane"],
-      price: 12,
       id: 1,
+      price: 12,
       image:"./img/denin-shorts.png",
   },
       {
       name: "Midi skirt",
+      brand:"Reiss",
+      size:"L",
       material: ["100% cotton"],
-      price: 12,
       id: 2,
+      price: 12,
       image:`img/skirt.png`
       
   }
